2491a94f355f627a70c063aef1e9e25cd69561a7
changjingxiu <changjingxiu@wondersgroup.com>
2017-04-14 10:06:46 +0800

    test

--- str.py
+++ str.py
@@ -58,6 +58,31 @@ class Solution:
 
         return A
 
+    def isPalindrome(self, s):
+        if not s:
+            return True
+        l, r = 0, len(s) - 1
+        while l < r:
+            # find left alphanumeric character
+            if not s[l].isalnum():
+                l += 1
+                continue
+            # find right alphanumeric character
+            if not s[r].isalnum():
+                r -= 1
+                continue
+            # case insensitive compare
+            if s[l].lower() == s[r].lower():
+                l += 1
+                r -= 1
+            else:
+                return False
+        return True
+
+
+
+
+
 if __name__ == '__main__':
     sol = Solution()
     print sol.common_long('changjingxiu','in')
